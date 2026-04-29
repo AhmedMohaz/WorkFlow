481728f546799019442918a1d6079a7a3eba272d
Ahmed Mohamed <itslokin5@gmail.com>
2026-04-29 18:50:42 +0300

    fix: include lambda function and update gitignore

---
+++ lambda/index.js
@@ -0,0 +1,19 @@
+const AWS = require('aws-sdk');
+const ssm = new AWS.SSM();
+
+exports.handler = async (event) => {
+    const params = {
+        Name: '/app/config/greeting',
+        WithDecryption: false
+    };
+    
+    const result = await ssm.getParameter(params).promise();
+    const greeting = result.Parameter.Value;
+    
+    console.log("Retrieved from SSM:", greeting);
+    
+    return {
+        status: "Success",
+        greeting: greeting
+    };
+};
